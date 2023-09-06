3f013047683d0845f0e709bb272e9e9793ea2205
Guilherme <guillhermmme@gmail.com>
2023-09-06 17:42:07 -0300

    init git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill } from "react-icons/bs";
+
+const API = "http://localhost:5000";
 
 function App() {
+  const [title, setTitle] = useState("")
+  const [time, setTime] = useState("")
+  const [todos, setTodos] = useState([])
+  const [loading, setLoading] = useState(false)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const todo = {
+      id: Math.random(),
+      title,
+      time,
+      done:false,
+    }
+
+    await fetch(API + "/todos", {
+      method: "POST",
+      body: JSON.stringify(todo),
+      headers: {
+        "Content-Type":"application/json",
+      },
+    });
+
+    setTitle("");
+    setTime("");
+    
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="todo-header">
+        <h1>React Todo</h1>
+      </div>
+      
+      <div className='form-todo'>
+        <h2>Insira a sua próxima tarefa:</h2>
+        <form onSubmit={handleSubmit}>
+        <div className='box-inputs'>
+        {/* -------------------- */}
+          <div className='form-control'>
+            <label htmlFor='title'>O que você vai fazer?</label>
+            <input
+              type='text'
+              id='title'
+              name='title'
+              placeholder='Título da tarefa'
+              onChange={(e) => setTitle(e.target.value)}
+              value={title || ""}
+              required
+            />
+          </div>
+        {/* ------------------ */}
+        <div className='form-control'>
+            <label htmlFor='time'>Duração:</label>
+            <input
+              type='time'
+              id='time'
+              name='time'
+              onChange={(e) => setTime(e.target.value)}
+              value={time || ""}
+              required
+            />
+          </div>
+        {/* ------------------ */}
+        </div>
+
+          <div className='btn_submit'>
+            <input type='submit' value="Enviar" />
+          </div>
+        </form>
+      </div>
+      <div className='list-todo'>
+        <h2>Lista de tarefas:</h2>
+        {todos.length === 0 && <p>Não há tarefas!</p>}
+      </div>
     </div>
   );
 }
